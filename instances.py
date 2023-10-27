94830af61d81da694ae1ed0c53a8564afaa95d6f
Subhajit Bhar <136987418+PyCoderX@users.noreply.github.com>
2023-10-27 13:21:23 +0530

    Update instances.py

--- instances.py
+++ instances.py
@@ -6,7 +6,6 @@ import telegram
 
 
 def is_current(fname: Path) -> bool:
-
     # if file dose not exist, return False
     if not Path.exists(fname):
         return False
@@ -16,8 +15,7 @@ def is_current(fname: Path) -> bool:
 
     # get file modification date
     modification_time = fname.stat().st_mtime
-    modification_date = datetime.datetime.fromtimestamp(
-        modification_time).date()
+    modification_date = datetime.datetime.fromtimestamp(modification_time).date()
 
     return modification_date == today
 
@@ -30,21 +28,21 @@ def get_instance(logdir: Path):
     if not is_current(telegramBot_fname):
         # initiate telegram Bot instance
         telegramBot = telegram.TelegramBot(log_path=logdir)
-        with open(telegramBot_fname, 'wb') as file:
+        with open(telegramBot_fname, "wb") as file:
             dill.dump(telegramBot, file, byref=False, recurse=True)
     else:
         # Export telegramBot
-        with open(telegramBot_fname, 'rb') as file:
+        with open(telegramBot_fname, "rb") as file:
             telegramBot = dill.load(file)
 
     if not is_current(fyers_fname):
         # initiate fyers login instance
-        fyers = fyersLogin.FyersLogin(log_path=logdir, teleBot=telegramBot)
+        fyers = fyersLogin.FyersLogin(log_path=logdir, teleBot=telegramBot).__call__()
         with open(fyers_fname, "wb") as file:
-            dill.dump(fyers(), file, byref=False, recurse=True)
+            dill.dump(fyers, file, byref=False, recurse=True)
     else:
         # Export fyers instance
-        with open(fyers_fname, 'rb') as file:
+        with open(fyers_fname, "rb") as file:
             fyers = dill.load(file)
 
     return telegramBot, fyers
