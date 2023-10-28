a993ac0fb6107b11bb9f7abf692916bcafe37f23
Subhajit Bhar <136987418+PyCoderX@users.noreply.github.com>
2023-10-28 10:49:24 +0530

    Update __init__.py
    added freezeargs

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,4 @@ from .rest_client import RestClient
 from .logger import ExceptionLogger
 from .measure_execution_time_decorator import MeasureExecutionTime
 from .retry_exception import retry
+from .freezeargs import freezeargs
