087aaa45a119e7af98b9d841cc9f6f70bdeb8ef7
Subhajit Bhar <136987418+PyCoderX@users.noreply.github.com>
2023-10-28 10:49:28 +0530

    Create freezeargs.py

---
+++ utils/freezeargs.py
@@ -0,0 +1,17 @@
+from frozendict import frozendict
+from functools import wraps
+
+
+def freezeargs(func):
+    """
+    Transform mutable dictionnary  Into immutable
+    Useful to be compatible with cache
+    """
+
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        args = tuple([frozendict(arg) if isinstance(arg, dict) else arg for arg in args])
+        kwargs = {k: frozendict(v) if isinstance(v, dict) else v for k, v in kwargs.items()}
+        return func(*args, **kwargs)
+
+    return wrapped
